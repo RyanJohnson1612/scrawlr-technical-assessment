ca448fc37de280b68310aeb591aebe43f65c68d1
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-03-28 13:15:30 -0400

    Remove old commented out code

--- src/components/UpvoteContainer.vue
+++ src/components/UpvoteContainer.vue
@@ -25,52 +25,19 @@
     section: Number
   });
 
+  // Increment upvotes when upvoteAdded is emitted
   const onUpvoteAdded = () => {
     upvotes.value++;
   }
 
+  // Update selected state in store when upvoteSelected is emitted
   const setSelected = () => store.commit('upvote/setSelected', props.section);
 
+  // Get selected state from store
   const selected = computed(() => {
     return store.getters['upvote/getSelected']
   });
 </script>
-
-<!--
-<script>
-import { ref, computed } from 'vue';
-import { useStore } from 'vuex';
-import AddUpvote from './AddUpvote.vue';
-import Upvote from './Upvote.vue';
-
-export default {
-  setup(props) {
-    const store = useStore();
-    const upvotes = ref(0);
-
-    const onUpvoteAdded = () => {
-      upvotes.value++;
-    }
-
-    return {
-      upvotes,
-      onUpvoteAdded,
-      selected: computed(() => store.getters['upvote/getSelected']),
-      setSelected: () => store.commit('upvote/setSelected', props.section)
-    }
-  },
-
-  props: {
-    section: Number
-  },
-
-  components: {
-    AddUpvote,
-    Upvote
-  }
-}
-</script>
--->
 
 <style>
   section {
